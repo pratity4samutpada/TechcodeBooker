340ce02de13dbe4ea33d51a89a7b6d8592b25d8c
shallinris <shallinris@gmail.com>
2017-03-05 16:40:57 +0200

    added block/endblock/extend to html docs

--- techcodebooker/roombooker/static/js/roombooker.js
+++ techcodebooker/roombooker/static/js/roombooker.js
@@ -1,3 +1,11 @@
 /**
  * Created by itc_user1 on 3/5/2017.
  */
+var imageArray = [];
+
+function roomImages() {
+    for (var i = 0; i < imageArray.length; i++) {
+        var image = $("<img/>").addClass("room_img");
+        $( ".room_div" ).append(image);
+    }
+}
