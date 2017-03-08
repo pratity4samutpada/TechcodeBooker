5f393e45fc53f41251e4341b73d467b4f3b0e6e1
Jordan Galler <jordan.galler@gmail.com>
2017-03-08 10:57:25 +0200

    Removed labels from booking fields in backend by overriding defaults.

--- techcodebooker/roombooker/forms.py
+++ techcodebooker/roombooker/forms.py
@@ -17,12 +17,18 @@ class room_form(forms.Form):
     room_id = forms.IntegerField(min_value=0,widget=forms.HiddenInput())
 
 class booking_form(forms.Form):
-    company= forms.ModelChoiceField(queryset=Companies.objects.all())
-    email = forms.EmailField()
-    booked_by = forms.CharField(max_length=200)
+    company= forms.ModelChoiceField(queryset=Companies.objects.all(),empty_label="Company")
+    email = forms.EmailField(widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    booked_by = forms.CharField(max_length=200,widget = forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Name'}))
     booking_date = forms.DateField(initial=datetime.date.today, widget=SelectDateWidget())
     start_time = forms.TimeField()
     end_time = forms.TimeField()
+
+    def __init__(self, *args, **kwargs):
+        super(booking_form, self).__init__(*args, **kwargs)
+        self.fields['email'].label = ""
+        self.fields['company'].label=""
+        self.fields['booked_by'].label=""
 
 class confirm_booking(forms.Form):
     status = forms.CharField(initial="confirmed", widget=forms.HiddenInput())
