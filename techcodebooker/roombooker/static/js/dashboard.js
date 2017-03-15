1193a2b480eff731e75bd4a9c5b375da1fd8b320
shallinris <shallinris@gmail.com>
2017-03-15 16:46:45 +0200

    added pending bookings table, rooms table and company table

---
+++ techcodebooker/roombooker/static/js/dashboard.js
@@ -0,0 +1,64 @@
+
+
+var Dashboard = {};
+
+Dashboard.init = function() {
+    $(document).ready(function() {
+        Dashboard.chooseView();
+        Dashboard.toggleRoomInfo();
+})
+};
+
+
+
+Dashboard.chooseView = function() {
+
+    $("#overview-button").click(function(){
+            $("#overview-view").show();
+            $("#bookings-view").hide();
+            $("#analytics-view").hide();
+    });
+
+    $("#bookings-button").click(function(){
+        $("#bookings-view").show();
+        $("#overview-view").hide();
+        $("#analytics-view").hide();
+    });
+
+    $("#analytics-button").click(function() {
+        $("#analytics-view").hide();
+        $("#overview-view").hide();
+        $("#bookings-view").hide();
+
+    })
+};
+
+Dashboard.toggleRoomInfo = function(roomId) {
+    $(".dashboard-edit-room-btns").click(function() {
+        $(".dashboard-rm-description").show()
+
+});
+
+        // $(".dashboard-rm-description").toggle('slow');
+    //    should be on click get room info from using ajax and append to room ie. on click send id of room to backend and gt info and return
+};
+
+
+
+Dashboard.init();
+
+// $.ajax({
+//             url: "getroominfo",
+//             method: "GET",
+//             data: {
+//                 id: roomId
+//             },
+//             success: function (result) {
+//                 var resFields = result[0].fields
+//                 var fac = resFields.room_fac
+//                 var cap = resFields.room_capacity
+//                 $("#apendto").text('Facilities : ' + fac)
+//                 $("#apendto").text('Room Capacity : ' + cap)
+//             }
+//
+//     })
